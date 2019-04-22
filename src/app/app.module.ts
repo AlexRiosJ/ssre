fd4ecb9b75ebc03fa4b5f50c21fcfa15b6e2e9af
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-22 11:15:14 -0500

    [Create] angular components. [Modified] routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { StudentComponent } from './student/student.component';
+import { SubjectComponent } from './subject-list/subject/subject.component';
+import { SubjectListComponent } from './subject-list/subject-list.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TimetableComponent,
+    StudentComponent,
+    SubjectComponent,
+    SubjectListComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
