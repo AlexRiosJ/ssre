a119a1e3032ff487580c4f7fb400d23295ec78e0
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-27 20:54:49 -0500

    [New] Landing Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent },
-  {path: 'home', component: HomeComponent },
-  {path: 'about', component: AboutComponent },
+  {path: '',  redirectTo: '/home', pathMatch: 'full',  canActivate: [AuthGuardService]},
+  {path: 'home', component: HomeComponent,  canActivate: [AuthGuardService] },
+  {path: 'landing', component: LandingComponent},
+  {path: 'about', component: AboutComponent},
 
 ];
 
