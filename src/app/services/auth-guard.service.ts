7f38031561cca55a1c346920ad610d1a123e5823
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-28 12:39:32 -0500

    [Modified] Landing page style

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -12,7 +12,8 @@ export class AuthGuardService implements CanActivate {
   route: ActivatedRouteSnapshot;
 
   constructor(private router: Router, private authService: AuthService) { }
-
+  // TODO: If the page is landing and the user is logged in, needs to redirect to home,
+  // in other words, user cannot go to landing page if the session is active
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     if (this.authService.isAuthenticated()) {
       return true;
