fa14e3a817aa3a7a839e9e6c2489ec0cb55bee16
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-27 22:12:45 -0500

    [Modified] Removing index component previously created

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn: boolean;
+  
+  constructor(auth: AuthService) {
+    this.isLoggedIn = auth.isAuthenticated();
+  }
 
   ngOnInit() {
   }
