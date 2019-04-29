7c218f64ade2367177f889b0dff48b4ca71a23d6
Carmen Martinez <carmen.mtzn@gmail.com>
2019-04-29 14:21:54 -0500

    [Modified] Change name

--- src/app/subject-list/GroupList.ts
+++ src/app/subject-list/GroupList.ts
@@ -2,10 +2,9 @@ import { Subject } from './subject/Subject';
 import { Teacher } from '../teacher/Teacher';
 import { ClassInformation } from './subject/ClassInformation';
 
-export class SubjectList {
+export class GroupList {
     constructor(
         public id: number,
-        public subject: Subject,
         public teacher: Teacher,
         public classInfo: ClassInformation[] // One object per day
         ) {}
